8d0f01437324b1fa08401dea3e1475948a080538
Dereje Desta <ddesta777@gmail.com>
2022-06-17 12:10:46 +0300

    Minor fixes

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,7 +4,7 @@ export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then(([photo, user]) => {
       console.log(photo.body, user.firstName, user.lastName);
-      return { ...photo, ...user };
+      return [photo, user];
     })
     .catch(() => console.log('Signup system offline'));
 }
--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,13 +2,5 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then(([user, photo]) => [
-      { status: 'rejected', value: user },
-      { status: 'rejected', value: photo },
-    ])
-    .catch((err) => [
-      { status: 'rejected', value: err.toString },
-      { status: 'rejected', value: err.toString },
-    ]);
+  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
 }
